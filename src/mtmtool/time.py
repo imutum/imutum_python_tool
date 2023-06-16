208c3dd1ef776e3c2a580c1d23207622e18a9005
imutum <50545012+imutum@users.noreply.github.com>
2023-06-16 19:27:17 +0800

    Update time.py
    自动从文本解析出时间，仅限于几个常用参数

--- src/mtmtool/time.py
+++ src/mtmtool/time.py
@@ -1,6 +1,6 @@
 import time
 import datetime, calendar
-
+import re
 
 class sTime:
     def __init__(self, srctime=None):
@@ -44,5 +44,26 @@ def timestr2timestamp(timestr, format="%Y-%m-%d %H:%M:%S", isMicroSecond=True):
     timeArray = time.strptime(timestr, format)
     return int(time.mktime(timeArray)) * (1000 if isMicroSecond else 1)
 
+def auto_parse_time_with_datefmt(timestr, datefmt):
+    pattern = datefmt.replace("%Y", "(\d{4})")\
+                    .replace("%j", "([0-2][0-9]{2}|3[0-5][0-9]|36[0-6])")\
+                    .replace("%m", "(0[1-9]|1[0-2])")\
+                    .replace("%d", "(0[1-9]|[1-2][0-9]|3[0-1])")\
+                    .replace("%H", "(0[1-9]|1[0-9]|2[0-4])")\
+                    .replace("%M", "([0-5][0-9])")\
+                    .replace("%S", "([0-5][0-9])")
+    pattern = f"{pattern}"
+    match = re.findall("(%[YjHMSmd])", datefmt)
+    if match is None:
+        return []
+    datefmt_new = "".join(match) if match else ""
+    match = re.findall(pattern, timestr)
+    datestr_new = ["".join(i) for i in match] if match else []
+    return [datetime.datetime.strptime(i, datefmt_new) for i in datestr_new]
 
 st = sTime()
+
+if __name__ == '__main__':
+    timestr = r"MCD19A2.A2019267.h28v05.006.2019269040927.hdf"
+    datefmt = "%Y%j"
+    print(auto_parse_time_with_datefmt(timestr, datefmt))
